df431e45c60248f5f859e7f0e81f10db928b30be
Paola04-byte <zp.unupn@uea.edu.ec>
2025-12-07 19:29:00 -0500

    SEMANA 2

---
+++ TECNICAS DE PROGRAMACIN/PROGRAMACION.py
@@ -0,0 +1,15 @@
+tecnicas-programacion-ejercicios/
+│
+├── tecnica_1_condicionales/
+│   ├── ejemplo1.py
+│   └── ejemplo2.py
+│
+├── tecnica_2_bucles/
+│   ├── ejemplo1.py
+│   └── ejemplo2.py
+│
+├── tecnica_3_funciones/
+│   ├── ejemplo1.py
+│   └── ejemplo2.py
+│
+└── README.md
